ededb367146f3cd232bb557d0e9473eca4be9640
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-02-15 21:13:48 -0300

    Add jinja template extensions

--- src/render_website.py
+++ src/render_website.py
@@ -2,6 +2,7 @@ from login_info import *
 import requests
 from jinja2 import Template
 import urllib.parse
+from jinja2 import Environment, FileSystemLoader
 
 
 def render_url(query):
@@ -43,16 +44,36 @@ missing_coins_query = missing_coins_query_template.render(
 )
 
 
-page_template_path = "./docs/index.html.jinja"
+page_template_path = "./docs/coin_by_group.html.jinja"
 
 
 with open(page_template_path) as f:
-    page_template = Template(f.read())
+    page_template_string = f.read()
 
+page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
+    page_template_string
+)
 
 webpage = page_template.render(
+    plant_family_name="all plants",
     collected_coins_query=render_url(collected_coins_query),
     missing_coins_query=render_url(missing_coins_query),
+)
+
+with open("docs/all.html", "w") as f:
+    f.write(webpage)
+
+with open("docs/index.html.jinja") as f:
+    home_string = f.read()
+
+webpage = (
+    Environment(loader=FileSystemLoader("docs/"))
+    .from_string(home_string)
+    .render(
+        plant_family_name="all plants",
+        collected_coins_query=render_url(collected_coins_query),
+        missing_coins_query=render_url(missing_coins_query),
+    )
 )
 
 with open("docs/index.html", "w") as f:
