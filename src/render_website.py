40c28b26a9191bf5a339e3e8ef243fc3755ddb59
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-04-09 12:18:36 -0300

    chore: Start changing paths to pathlib

--- src/render_website.py
+++ src/render_website.py
@@ -2,140 +2,131 @@ from login_info import *
 from helper import *
 from wikidata2df import wikidata2df
 import json
+from pathlib import Path
 
-endpoint = "https://api.numista.com/api/v2"
-user_id = "231967"
-response = requests.get(
-    endpoint + "/users/" + user_id + "/collected_coins",
-    params={"lang": "en"},
-    headers={"Numista-API-Key": api_key},
-)
-user_details = response.json()
-collected_coins = user_details["collected_coins"]
-
-with open("data/coin_images.json") as f:
-    coin_images = json.loads(f.read())
-
-page_template_path = "./docs/all.html.jinja"
-with open(page_template_path) as f:
-    page_template_string = f.read()
-
-page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
-    page_template_string
-)
-
-webpage = page_template.render(coins=coin_images, title="All coins")
-
-with open("docs/all.html", "w") as f:
-    f.write(webpage)
+HERE = Path(__file__).parent.resolve()
 
 
-with open("docs/index.html.jinja") as f:
-    page_template_string = f.read()
+def main():
+    data_path = HERE.parents[0] / "data"
+    docs_path = HERE.parents[0] / "docs"
+    queries_path = HERE / "queries"
 
-page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
-    page_template_string
-)
+    endpoint = "https://api.numista.com/api/v2"
+    user_id = "231967"
+    response = requests.get(
+        endpoint + "/users/" + user_id + "/collected_coins",
+        params={"lang": "en"},
+        headers={"Numista-API-Key": api_key},
+    )
+    user_details = response.json()
+    collected_coins = user_details["collected_coins"]
 
-webpage = page_template.render()
+    coin_images_path = data_path / "coin_images.json"
+    coin_images = json.loads(coin_images_path.read_text())
 
-with open("docs/index.html", "w") as f:
-    f.write(webpage)
-
-
-# Render By Family pages
-
-with open("src/queries/get_families.rq") as f:
-    query = f.read()
-
-
-families_df = wikidata2df(query)
-print(families_df)
-plant_families = []
-seen = []
-for i, row in families_df.iterrows():
-    if row["family"] in seen:
-        continue
-    else:
-        seen.append(row["family"])
-    plant_families.append(
-        {
-            "lowercase_name": row["family_name"].lower(),
-            "name": row["family_name"],
-            "qid": row["family"],
-            "pic_urls": row["pics"].split("|"),
-        }
+    p = docs_path / "all.html.jinja"
+    page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
+        p.read_text()
     )
 
-print(plant_families)
+    webpage = page_template.render(coins=coin_images, title="All coins")
 
+    p = docs_path / "all.html"
+    p.write_text(webpage)
 
-with open("docs/family.html.jinja") as f:
-    page_template_string = f.read()
-
-page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
-    page_template_string
-)
-
-webpage = page_template.render(plant_families=plant_families)
-
-with open("docs/family.html", "w") as f:
-    f.write(webpage)
-
-for plant_family in plant_families:
-    generate_plant_html(
-        collected_coins,
-        qid=plant_family["qid"],
-        name=plant_family["name"],
-        base_class="family",
+    p = docs_path / "index.html.jinja"
+    page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
+        p.read_text()
     )
 
-# Render By Country pages
+    webpage = page_template.render()
 
-with open("src/queries/get_countries.rq") as f:
-    query = f.read()
+    p = docs_path / "index.html"
+    p.write_text(webpage)
 
-countries_df = wikidata2df(query)
-print(countries_df)
-countries = []
-seen = []
-for i, row in countries_df.iterrows():
-    if row["country"] in seen:
-        continue
-    else:
-        seen.append(row["country"])
-    countries.append(
-        {
-            "lowercase_name": row["countryLabel"].lower(),
-            "name": row["countryLabel"],
-            "qid": row["country"],
-            "pic_urls": row["pics"].split("|"),
-        }
+    # Render By Family pages
+
+    p = queries_path / "get_families.rq"
+    query = p.read_text()
+    families_df = wikidata2df(query)
+
+    plant_families = []
+    seen = []
+    for i, row in families_df.iterrows():
+        if row["family"] in seen:
+            continue
+        else:
+            seen.append(row["family"])
+        plant_families.append(
+            {
+                "lowercase_name": row["family_name"].lower(),
+                "name": row["family_name"],
+                "qid": row["family"],
+                "pic_urls": row["pics"].split("|"),
+            }
+        )
+
+    p = docs_path / "family.html.jinja"
+    page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
+        p.read_text()
     )
+    webpage = page_template.render(plant_families=plant_families)
 
-with open("docs/country.html.jinja") as f:
-    page_template_string = f.read()
-page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
-    page_template_string
-)
-webpage = page_template.render(countries=countries)
-print(webpage)
-with open("docs/country.html", "w") as f:
-    f.write(webpage)
+    p = docs_path / "family.html"
+    p.write_text(webpage)
 
-with open("docs/family.html.jinja") as f:
-    page_template_string = f.read()
+    for plant_family in plant_families:
+        generate_plant_html(
+            collected_coins,
+            qid=plant_family["qid"],
+            name=plant_family["name"],
+            base_class="family",
+        )
 
-page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
-    page_template_string
-)
+    # Render By Country pages
+    p = queries_path / "get_countries.rq"
+    query = p.read_text()
 
-webpage = page_template.render(plant_families=plant_families)
+    countries_df = wikidata2df(query)
+    countries = []
+    seen = []
+    for i, row in countries_df.iterrows():
+        if row["country"] in seen:
+            continue
+        else:
+            seen.append(row["country"])
+        countries.append(
+            {
+                "lowercase_name": row["countryLabel"].lower(),
+                "name": row["countryLabel"],
+                "qid": row["country"],
+                "pic_urls": row["pics"].split("|"),
+            }
+        )
 
-for country in countries:
-    generate_plant_html(
-        collected_coins,
-        qid=country["qid"],
-        name=country["name"],
-        base_class="country",
+    p = docs_path / "country.html.jinja"
+    page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
+        p.read_text()
     )
+    webpage = page_template.render(countries=countries)
+    p = docs_path / "country.html"
+    p.write_text(webpage)
+
+    p = docs_path / "country.html.jinja"
+    page_template = Environment(loader=FileSystemLoader("docs/")).from_string(
+        p.read_text()
+    )
+    webpage = page_template.render(plant_families=plant_families)
+
+    for country in countries:
+        generate_plant_html(
+            collected_coins,
+            qid=country["qid"],
+            name=country["name"],
+            base_class="country",
+        )
+
+
+if __name__ == "__main__":
+    main()
